b66a21a9939f10688f3ed6e1143ba4be1133f177
Mrpolat <m.polat.ucar@gmail.com>
2022-04-04 13:38:09 +0300

    Modal Done

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,28 +1,31 @@
 import React from 'react'
-import {MdClose} from 'react-icons/md'
+import { MdClose } from 'react-icons/md'
 import { useAction } from '../../context/action';
 
-const Modal=()=> {
+const Modal = () => {
 
-  const {showModal,setModal} = useAction();
+  const { showModal, setModal } = useAction();
   return (
-      <>{showModal? (
-        <div className='jfModal-backGround'>
-            <div className='jfModal-modalWrapper' >
-                <div className='jfModal-modalContent'>
-                  <label className='userLoginLabel' htmlFor="">UserName:</label>
-                  <input className='jfuserLoginInput' type="text" />
-                  <br />
-                  <label className='userLoginLabel' htmlFor="">Password:</label>
-                  <input className='jfuserLoginInput' type="text" />
-                    <div className='jfModal-closeModalButton' aria-label='close modal' onClick={()=> setModal(false)}>
-                        <MdClose/>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <>{showModal ? (
+      <div className='jfModal-backGround'>
+        <div className='jfModal-modalWrapper' >
+          <div className='jfModal-modalContent'>
+            <div className='jfuserLogin'>
+              <label className='jfuserLogin-item jfuserLogin-label' htmlFor="">Username or Email</label>
+              <input className='jfuserLogin-item jfuserLogin-input' type="email" name='email' />
 
-      ):null}
+              <label className='jfuserLogin-item jfuserLogin-label'  htmlFor="">Password</label>
+              <input className='jfuserLogin-item jfuserLogin-input' type="password" name='password'  />
+              <button className='jfuserLogin-item jfuserLogin-button'>Giriş</button>
+              </div>
+              <div className='jfModal-closeModalButton' aria-label='close modal' onClick={() => setModal(false)}>
+              <MdClose />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    ) : null}
     </>
   )
 }
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -3,22 +3,27 @@ import { useSub } from '../../context/submission';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
 const SideBar = () => {
-  
-  const {submissions} = useSub();  
 
-  
+  const { submissions } = useSub();
+
+
   return (
     <div className='jfSideBar'>
       {
-      submissions.map(sub=> 
-      <Link 
-      to={sub.id} 
-      key={sub.id} 
-      className={classnames('jfSideBar-item', sub.answers[11].answer === 'Derelict' && 'jfSideBar-derelict')}>
-        <div className='jfSideBar-item-pad'>
-        {sub.answers[7].answer}
-        </div>
-        </Link>)
+        submissions.map(sub =>
+          <Link
+            to={sub.id}
+            key={sub.id}
+            className={classnames('jfSideBar-item', sub.answers[11].answer === 'Derelict' && 'jfSideBar-item')}>
+            <div className='jfSideBar-item-pad'>
+               <span className='jfSideBar-item-petName'>
+              {sub.answers[7].answer} 
+              </span>
+              <span  className='jfSideBar-item-ownerName'>        
+                {sub.answers[8].answer.first + " " + sub.answers[8].answer.last}
+              </span>
+            </div>
+          </Link>)
       }
     </div>
   )
